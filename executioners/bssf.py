be73d5b70f0d3984c3df5e3e91aaf385d484290e
SinuSignal <guillesanchillan@gmail.com>
2025-02-06 18:30:00 +0100

    more induce slice

--- executioners/bssf.py
+++ executioners/bssf.py
@@ -18,6 +18,7 @@ Bssf logic for the intent engine
 import json
 import logging
 
+from devtools import pprint
 from queue import Queue
 # from intent_engine.executioners.http_handler import http_handler
 # from intent_engine.core import ib_model
@@ -51,44 +52,45 @@ class bssf():
         print("Processing bssf")
         print("Request: %s",request)
 
-        intent_data=request
+        slice_data=request
 
         nodes=[]
         links=[]
         applications=[]
         slices=[]
         intent={}
-        contexts=[]
+        objectContexts=[]
         expectations=[]
         id=0
-        
-        if(intent_data['componentNodeInstances']):
-            for node in intent_data['componentNodeInstances']:
+
+        # Nodes definition
+        if('componentNodeInstances' in slice_data):
+            for node in slice_data['componentNodeInstances']:
                 logger.debug("Node: %s",node)
-                if(node['componentNodeInstanceID']):
+                print("Node: %s",node)
+                objectContexts=[]
+                expectation={}
+                if('componentNodeInstanceID' in node):
                     logger.debug("BSSF node")
-                    context={
+                    objectContexts.append({
                         'contextAttribute':'componentNodeInstanceID',
                         'contextCondition':'IS_EQUAL_TO',
                         'contextValue':node['componentNodeInstanceID']
-                    }
-                    contexts.append(context)
-                if(node['componentNodeInstanceName']):
-                    context={
+                    })
+                if('componentNodeInstanceName' in node):
+                    objectContexts.append({
                         'contextAttribute':'componentNodeInstanceName',
                         'contextCondition':'IS_EQUAL_TO',
                         'contextValue':node['componentNodeInstanceName']
-                    }
-                    contexts.append(context)
-                if(node['componentNodeInstanceHexID']):
-                    context={
+                    })
+                if('componentNodeInstanceHexID' in node):
+                    objectContexts.append({
                         'contextAttribute':'componentNodeInstanceHexID',
                         'contextCondition':'IS_EQUAL_TO',
                         'contextValue':node['componentNodeInstanceHexID']
-                    }
-                    contexts.append(context)
+                    })
                 expectation_object={
-                    'context':contexts,
+                    'objectContexts':objectContexts,
                     'objectType':'node',
                     'objectInstance':node['componentNodeInstanceID']
                 }
@@ -100,6 +102,163 @@ class bssf():
                 id+=1
                 expectations.append(expectation)
 
+        # Links definition
+        if('graphLinkNodeInstances' in slice_data):
+            for link in slice_data['graphLinkNodeInstances']:
+                objectContexts=[]
+                expectation={}
+                if('graphLinkNodeInstanceID' in link):
+                    objectContexts.append({
+                        'contextAttribute':'graphLinkNodeInstanceID',
+                        'contextCondition':'IS_EQUAL_TO',
+                        'contextValue':link['graphLinkNodeInstanceID']
+                    })
+                if('fromComponentNodeInstanceID' in link):
+                    objectContexts.append({
+                        'contextAttribute':'fromComponentNodeInstanceID',
+                        'contextCondition':'IS_EQUAL_TO',
+                        'contextValue':link['fromComponentNodeInstanceID']
+                    })
+                if('toComponentNodeInstanceID' in link):
+                    objectContexts.append({
+                        'contextAttribute':'toComponentNodeInstanceID',
+                        'contextCondition':'IS_EQUAL_TO',
+                        'contextValue':link['toComponentNodeInstanceID']
+                    })
+                if('type' in link):
+                    objectContexts.append({
+                        'contextAttribute':'type',
+                        'contextCondition':'IS_EQUAL_TO',
+                        'contextValue':link['type']
+                    })
+                expectation_object={
+                    'objectType':'6green_slice',
+                    'objectInstance':link['graphLinkNodeInstanceID'],
+                    'objectContexts':objectContexts
+                }
+                expectation={
+                    'expectationId':id+1,
+                    'expectationVerb':'DELIVER',
+                    'expectationObject':expectation_object
+                }
+                id+=1
+                expectations.append(expectation)
+        
+        intent['intentExpectations']=expectations
+
+        # Constrains for nodes
+        if 'constraints' in slice_data:
+            for constraint in slice_data['constraints']:
+                objectContexts=[]
+                expectation={}
+                if('componentNodeInstanceID' in constraint):
+                # Computing constrains for nodes
+                # Look for the node in the nodes list to add this target
+                    for expectation in intent['intentExpectations']:
+                        for objectContext in expectation['expectationObject']['objectContexts']:
+                            if objectContext['contextAttribute']=='componentNodeInstanceID':
+                                if objectContext['contextValue']==constraint['componentNodeInstanceID']:
+                                    targetContexts=[]
+                                    targetContexts.append({
+                                        'contextAttribute':'category',
+                                        'contextCondition':'IS_EQUAL_TO',
+                                        'contextValue':constraint['category']
+                                    })
+                                    targetContexts.append({
+                                        'contextAttribute':'type',
+                                        'contextCondition':'IS_EQUAL_TO',
+                                        'contextValue':constraint['type']
+                                    })
+                                    if 'expectationTargets' not in expectation['expectationObject']:
+                                        expectation['expectationObject']['expectationTargets']=[]
+
+                                    expectation['expectationObject']['expectationTargets'].append({
+                                        'targetName': constraint['constraintID'],
+                                        'targetCondition': 'IS_EQUALTO', # TODO: function to decided conditions
+                                        'targetValue': constraint['constraintValue']+" "+constraint['constraintUnit'],
+                                        'targetContexts': targetContexts
+                                    })
+                if('interfaceInstanceID' in constraint):
+                    # Computing constrains for links --> slices
+                    # Create an expectation for the slice with the constraints as targets
+                    objectContexts=[]
+                    expectation={}
+                    expectationTargets=[]
+                    expectationContexts=[]
+                    objectContexts.append({
+                        'contextAttribute':'interfaceInstanceID',
+                        'contextCondition':'IS_EQUAL_TO',
+                        'contextValue':constraint['interfaceInstanceID']
+                    })
+                    if 'resourceType' in constraint:
+                        objectContexts.append({
+                            'contextAttribute':'resourceType',
+                            'contextCondition':'IS_EQUAL_TO',
+                            'contextValue':constraint['resourceType']
+                        })
+                    if 'radioServiceType' in constraint:
+                        objectContexts.append({
+                            'contextAttribute':'radioServiceType',
+                            'contextCondition':'IS_EQUAL_TO',
+                            'contextValue':constraint['radioServiceType']
+                        })
+                    if 'allocationRetentionPriorityProfile' in constraint:
+                        objectContexts.append({
+                            'contextAttribute':'allocationRetentionPriorityProfile',
+                            'contextCondition':'IS_EQUAL_TO',
+                            'contextValue':constraint['allocationRetentionPriorityProfile']
+                        })
+                    if 'minimumGuaranteedBandwidth' in constraint:
+                        expectationTargets.append({
+                            'targetAttribute':'minimumGuaranteedBandwidth',
+                            'targetCondition':'IS_HIGHER_THAN',
+                            'targetValue':str(constraint['minimumGuaranteedBandwidth'])+" "+constraint['constraintUnit']
+                        })
+                    if 'maximumRequiredBandwidth' in constraint:
+                        expectationTargets.append({
+                            'targetAttribute':'maximumRequiredBandwidth',
+                            'targetCondition':'IS_LOWER_THAN',
+                            'targetValue':str(constraint['maximumRequiredBandwidth'])+" "+constraint['constraintUnit']
+                        })
+                    if 'category' in constraint:
+                        expectationContexts.append({
+                            'contextAttribute':'category',
+                            'contextCondition':'IS_EQUAL_TO',
+                            'contextValue':constraint['category']
+                        })
+                    if 'type' in constraint:
+                        expectationContexts.append({
+                            'contextAttribute':'type',
+                            'contextCondition':'IS_EQUAL_TO',
+                            'contextValue':constraint['type']
+                        })
+                    if 'qi' in constraint:
+                        expectationTargets.append({
+                            'targetAttribute':'qi',
+                            'targetCondition':'IS_EQUAL_TO',
+                            'targetValue':constraint['qi']
+                        })
+                    intent_expectation={
+                        'expectationId':id+1,
+                        'expectationObject':{
+                            'objectType':'interfaceInstance',
+                            'objectInstance':constraint['interfaceInstanceID'],
+                            'objectContexts':objectContexts},
+                        'expectationTargets': expectationTargets,
+                        'expectationContexts':expectationContexts
+                    }
+                    id+=1
+                    expectations.append(intent_expectation)
+
+        # Intent contexts
+        
+
+        intent['intentId']=slice_data['applicationInstanceID']
+        intent['userLabel']=slice_data['name']
+        intent['intentAdminState']='PENDING'
+
+        pprint(intent)
+        
         intent=self.data_to_intent(request)
         self.send_to_intent_queue(intent)
         return True
@@ -127,6 +286,12 @@ class bssf():
             logger.debug("BSSF http")
             self.http_handler.execute(data_and_params)
 
+def check_key(dic, key):
+    if key in dic.keys():
+        print("Present, ", end =" ")
+        print("value =", dic[key])
+    else:
+        print("Not present")
 
 # Example usage
 if __name__ == "__main__":
